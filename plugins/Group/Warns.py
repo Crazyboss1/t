0c7203c35a67207838754ec8a2ee9a55667a5814
Joel Kurian Biju <92435259+Joelkb@users.noreply.github.com>
2022-09-25 12:01:48 +0530

    Update Warns.py

--- plugins/Group/Warns.py
+++ plugins/Group/Warns.py
@@ -250,7 +250,7 @@ async def remove_last_warn_btn(c: Tiger, q: CallbackQuery):
     user_id = int(args[2])
     chat_id = int(q.message.chat.id)
     user = Users.get_user_info(int(user_id))
-    user_first_name = Users.self.user_info["name"]
+    user_first_name = user["name"]
 
     if action == "remove":
         warn_db = Warns(q.message.chat.id)
