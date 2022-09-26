089f262a7bd4ad0c96b17b71afb62406852f0cfd
Naveen <93958628+Naveen-TG@users.noreply.github.com>
2022-09-26 19:20:27 +0530

    Create pyupload.py

---
+++ plugins/pyupload.py
@@ -0,0 +1,38 @@
+from kashmira import tbot 
+import os
+import asyncio
+import os
+import time
+
+from info import tbot
+from plugins.helper_funcs.events import register
+
+OWNER_ID = 2107036689
+
+from datetime import datetime
+path = TEMP_DOWNLOAD_DIRECTORY = "./"
+water = "./kashmira/admins/photo_2222-07-01_15-044-19.jpg"
+client = tbot
+
+@register(pattern=r"^/pyupload ?(.*)")
+async def Prof(event):
+    if event.sender_id == OWNER_ID or event.sender_id == DEV_USERS:
+        pass
+    else:
+        return
+    thumb = water
+    message_id = event.message.id
+    input_str = event.pattern_match.group(1)
+    the_plugin_file = "./kashmira/modules/{}.py".format(input_str)
+    if os.path.exists(the_plugin_file):
+     message_id = event.message.id
+     await event.client.send_file(
+             event.chat_id,
+             the_plugin_file,
+             force_document=True,
+             allow_cache=False,
+             thumb=thumb,
+             reply_to=message_id,
+         )
+    else:
+        await event.reply("**No File Found!**")
