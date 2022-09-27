480484e9a3892dacb6d0d290fe939d9266a9a823
༒ᶜʳᵃᶻʸᴮᴼˢˢ卂乃卄丨丂卄乇Ҝ༒ <93928612+200920082007@users.noreply.github.com>
2022-09-27 20:54:32 +0530

    Create calc.py

---
+++ plugins/CrazyBoss/calc.py
@@ -0,0 +1,52 @@
+"""
+Fetch App Details from Playstore.
+.app <app_name> to fetch app details.
+  © [cHAuHaN](http://t.me/amnd33p)
+"""
+
+import io
+import sys
+import traceback
+from plugins.helper_functions import register
+
+plugin_category = "utils"
+
+
+@register(pattern=r"^/calc ?(.*)")
+async def calculator(event):
+    "To solve basic mathematics equations."
+    cmd = event.text.split(" ", maxsplit=1)[1]
+    event = await reply_text(event, "Calculating ...")
+    old_stderr = sys.stderr
+    old_stdout = sys.stdout
+    redirected_output = sys.stdout = io.StringIO()
+    redirected_error = sys.stderr = io.StringIO()
+    stdout, stderr, exc = None, None, None
+    san = f"print({cmd})"
+    try:
+        await aexec(san, event)
+    except Exception:
+        exc = traceback.format_exc()
+    stdout = redirected_output.getvalue()
+    stderr = redirected_error.getvalue()
+    sys.stdout = old_stdout
+    sys.stderr = old_stderr
+    evaluation = ""
+    if exc:
+        evaluation = exc
+    elif stderr:
+        evaluation = stderr
+    elif stdout:
+        evaluation = stdout
+    else:
+        evaluation = "Sorry I can't find result for the given equation"
+    final_output = "**EQUATION**: `{}` \n\n **SOLUTION**: \n`{}` \n".format(
+        cmd, evaluation
+    )
+    await event.edit(final_output)
+
+
+async def aexec(code, event):
+    exec("async def __aexec(event): " + "".join(f"\n {l}" for l in code.split("\n")))
+
+    return await locals()["__aexec"](event)
