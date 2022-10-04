6146e2e72ca4633e1c3a42fb141ca68fe5849b7c
༒ᶜʳᵃᶻʸᴮᴼˢˢ卂乃卄丨丂卄乇Ҝ༒ <93928612+200920082007@users.noreply.github.com>
2022-10-04 09:02:48 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -76,7 +76,7 @@ BUTTONS = {}
 SPELL_CHECK = {}
 CHANNEL_ID = int(-1001161499656)
 
-@Client.on_message(filters.group & filters.text & ~filters.edited & filters.incoming)
+@Client.on_message(filters.group & filters.text & filters.incoming)
 async def give_filter(client, message):
     k = await manual_filters(client, message)
     if k == False:
